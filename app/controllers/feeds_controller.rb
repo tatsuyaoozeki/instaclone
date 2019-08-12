d12b385375376880147dcb18af6eeca1be36b030
tatsuya oozeki <tasuya6281@icloud.com>
2019-08-12 15:35:45 +0900

    newとeditをパーシャル化

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,4 +1,6 @@
 class FeedsController < ApplicationController
+  before_action :set_feed, only: [:show, :edit, :destroy]
+  
   def index
     @feeds = Feed.all
   end
@@ -8,7 +10,7 @@ class FeedsController < ApplicationController
   end
 
   def create
-    @feed = Feed.create(feed_params)
+    @feed = Feed.new(feed_params)
     if @feed.save
       redirect_to feeds_path, notice: "作成しました！"
     else
@@ -20,10 +22,27 @@ class FeedsController < ApplicationController
     @feed = Feed.find(params[:id])
   end
 
+  def edit
+    @feed = Feed.find(params[:id])
+  end
+
+  def update
+    @feed = Feed.find(params[:id])
+    if @feed.update(feed_params)
+      redirect_to feeds_path, notice: "編集しました"
+    else
+      render :edit
+    end
+  end
+
 
   private
 
   def feed_params
     params.require(:feed).permit(:content, :image, :image_cache)
+  end
+
+  def set_feed
+    @feed = Feed.find(params[:id])
   end
 end
