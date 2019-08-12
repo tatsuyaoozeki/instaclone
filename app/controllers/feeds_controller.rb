f6d07bc1c19368ea3e31b7bf8a957bd5bc21a25a
tatsuya oozeki <tasuya6281@icloud.com>
2019-08-12 14:36:14 +0900

    投稿機能作成

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,5 +3,18 @@ class FeedsController < ApplicationController
   end
 
   def new
+    @feed = Feed.new
+  end
+
+  def create
+    Feed.create(feed_params)
+    redirect_to new_feed_path
+  end
+
+
+  private
+
+  def feed_params
+    params.require(:feed).permit(:content, :image, :image_cache)
   end
 end
